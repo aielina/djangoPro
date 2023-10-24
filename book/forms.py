236cf2fdf310ba29a0be066d660d65356fe5b37d
aielina <aielinazhaparbekova@gmail.com>
2023-10-24 14:18:40 +0600

    create delete

--- book/forms.py
+++ book/forms.py
@@ -5,3 +5,8 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = models.ReviewBook
         fields = '__all__'
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = models.Book
+        fields = '__all__'
--- book/urls.py
+++ book/urls.py
@@ -5,5 +5,11 @@ urlpatterns = [
     path('book', views.book_view),
     path('book_detail/<int:id>', views.book_detail_view, name='book_detail'),
     path('', views.home, name='home'),
+
+    path('lang_list/', views.book_delete_view),
+    path('lang_list/<int:id>/delete/', views.book_drop_view),
+    path('create_post_lang/', views.createLangPostView),
+
+    path('add-cooment/', views.createBookView),
     path('add-cooment/', views.createBookView),
 ]
